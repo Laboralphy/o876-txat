2e59309fbec034cf655ada23a55237bb012c4c50
ralphy <raphael.marandet@gmail.com>
2025-03-17 16:00:15 +0100

    add user rank const and date provider interface

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
     TxatManager: require('./src/TxatManager'),
     EVENT_TYPES: require('./src/data/event-types.json'),
+    USER_RANKS: require('./src/data/user-ranks.json'),
+    IDateProvider: require('./src/date-provider/IDateProvider')
 };
